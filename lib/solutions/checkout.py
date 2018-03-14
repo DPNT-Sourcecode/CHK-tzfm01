4e8b3d826b63cac15ddefd01147de4209db6bf2e
sbx_user1059 <sbx_user1059@ip-10-11-30-249.eu-west-2.compute.internal>
2018-03-14 16:38:35 +0000

    Wed Mar 14 16:38:35 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -6,7 +6,7 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
     skus = skus.upper()
-    skus_copy = skus
+    skus_copy = skus.upper()
     if skus == ' ':
         # if it's a string with a space
         return -1
@@ -32,5 +32,9 @@ def checkout(skus):
     if Bs > 0:
         bs_sum += (Bs // 2) * 45
         bs_sum += (Bs % 2) * 30
-    return Cs + Ds + bs_sum +  as_sum
+    print 'As', as_sum
+    print 'Bs', bs_sum
+    print 'Cs', Cs
+    print 'Ds', Ds
+    return Cs + Ds + bs_sum + as_sum
 
