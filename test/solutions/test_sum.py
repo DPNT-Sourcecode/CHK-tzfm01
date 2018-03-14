4fcdb41d6cde8bf0f107bff645cbaa5c6342ec46
sbx_user1059 <sbx_user1059@ip-10-11-30-249.eu-west-2.compute.internal>
2018-03-14 16:07:50 +0000

    Wed Mar 14 16:07:50 UTC 2018

--- test/solutions/test_sum.py
+++ test/solutions/test_sum.py
@@ -1,11 +1,17 @@
 import unittest
 
 from lib.solutions.sum import sum
+from lib.solutions.hello import hello
 
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum(1, 2), 3)
+
+
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello('Mihai'), 'Hello, Mihai!')
 
 
 if __name__ == '__main__':
