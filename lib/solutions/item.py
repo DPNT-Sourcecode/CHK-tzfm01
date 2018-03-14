80ab6bb55fb4261697082f335814037b230bd80c
sbx_user1059 <sbx_user1059@ip-10-11-30-249.eu-west-2.compute.internal>
2018-03-14 19:18:17 +0000

    Wed Mar 14 19:18:17 UTC 2018

--- lib/solutions/item.py
+++ lib/solutions/item.py
@@ -5,8 +5,28 @@ class Item(object):
     def __init__(self, price, offers):
         self.price = price
         self.offers = offers
-        self.quantity = 0
-        self.totat_price = 0
+        self._quantity = 0
+        self.total_price = 0
         self.parent = []
 
-    def 
+    @quantity.setter
+    def quantity(self, value):
+        self._quantity = value
+
+    @property
+    def quantity(self):
+        return self.quantity
+
+    def update_quantity(self, n):
+        self._quantity -= n
+
+    def itemx(self, x, group_price):
+        """
+        Offers like  1 for 3£, 2 for 5£
+        :param x:
+        :param group_price:
+        :return:
+        """
+        included_offers = self._quantity // x
+        self._quantity = self._quantity % x
+        self.total_price += included_offers * group_price
