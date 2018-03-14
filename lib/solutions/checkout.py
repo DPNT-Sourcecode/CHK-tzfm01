8f155f23231ed8b573e56f12610308730486071e
sbx_user1059 <sbx_user1059@ip-10-11-30-249.eu-west-2.compute.internal>
2018-03-14 16:26:29 +0000

    Wed Mar 14 16:26:29 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -13,11 +13,13 @@ def checkout(skus):
         return -1
     skus = skus.upper()
     As = skus.count('A')
-    batches_As = As // 3
-    individual_As = As % 3
+    batches_As = (As // 3) * 130
+    individual_As = (As % 3) * 50
     Bs = skus.count('B')
+    batches_Bs = (Bs // 2) * 45
+    individual_Bs = (Bs % 2) * 30
     Cs = skus.count('C') * 20
     Ds = skus.count('D') * 15
 
-    return 30
+    return Cs + Ds + batches_As + individual_As + batches_Bs + individual_Bs
 
