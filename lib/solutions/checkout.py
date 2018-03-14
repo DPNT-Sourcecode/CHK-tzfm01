213bf191a51ba873033237d9cb24b3970616564b
sbx_user1059 <sbx_user1059@ip-10-11-30-249.eu-west-2.compute.internal>
2018-03-14 16:25:24 +0000

    Wed Mar 14 16:25:24 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,5 +5,19 @@
 def checkout(skus):
     if not isinstance(skus, str):
         return -1
+    if not skus:
+        # if it's null string
+        return -1
+    if skus == ' ':
+        # if it's a string with a space
+        return -1
+    skus = skus.upper()
+    As = skus.count('A')
+    batches_As = As // 3
+    individual_As = As % 3
+    Bs = skus.count('B')
+    Cs = skus.count('C') * 20
+    Ds = skus.count('D') * 15
+
     return 30
 
