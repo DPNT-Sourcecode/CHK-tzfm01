4eaafa6de5280cd0d3a1925fe9d16747935934ed
sbx_user1059 <sbx_user1059@ip-10-11-30-249.eu-west-2.compute.internal>
2018-03-14 19:27:17 +0000

    Wed Mar 14 19:27:17 UTC 2018

--- lib/solutions/item.py
+++ lib/solutions/item.py
@@ -2,7 +2,8 @@ class Item(object):
     """
     This is a item, product from the supermarket basket
     """
-    def __init__(self, price, offers):
+    def __init__(self, item, price, offers):
+        self.type = item
         self.price = price
         self.offers = offers
         self._quantity = 0
@@ -19,6 +20,11 @@ class Item(object):
 
     def update_quantity(self, n):
         self._quantity -= n
+        # this is from outside; should recalculate the price when this happens
+        # self.calculate_price()
+
+    def individual_price(self):
+        self.total_price += self._quantity * self.price
 
     def itemx(self, x, group_price):
         """
@@ -30,3 +36,31 @@ class Item(object):
         included_offers = self._quantity // x
         self._quantity = self._quantity % x
         self.total_price += included_offers * group_price
+
+    def one_for_free(self, x):
+        """
+        Offers like buy 3 get one free
+        :param x:
+        :return:
+        """
+        included_offers = self._quantity // (x + 1)
+        self._quantity = self._quantity % (x + 1)
+        self.total_price += included_offers * x * self.price
+
+    def nX_Y_free(self, n, y):
+        """
+        Offers like 3R get one Q free
+        :param n:
+        :param y:
+        :return:
+        """
+        included_offers = self._quantity // n
+        for item in self.parent:
+            if item.type == y:
+                item.update_quantity(included_offers)
+
+    def calculate_parents(self):
+        """
+        Match rules like 3R get one Q free [\]
+        :return:
+        """
